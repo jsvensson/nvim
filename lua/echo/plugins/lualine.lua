585ec8cc21c056de0b2a65f46d8e274d095c1694
Johan Svensson <johan@reducer.io>
2023-09-11 01:09:04 +0200

    start customizing lualine

--- lua/echo/plugins/lualine.lua
+++ lua/echo/plugins/lualine.lua
@@ -1,28 +1,43 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+local M = {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local lualine = require("lualine")
+    local lualine = require('lualine')
 
     -- configure lazy pending update count
-    local lazy_status = require("lazy.status")
+    local lazy_status = require('lazy.status')
 
     lualine.setup({
       extensions = {
         "neo-tree",
       },
       sections = {
-       lualine_x = {
+        lualine_a = {
+          'mode',
+        },
+        lualine_b = {
+          { 'branch',
+            icon = '',
+          },
+          'diff',
+          'diagnostics',
+        },
+        lualine_c = {
+          'filename',
+        },
+        lualine_x = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            color = { fg = '#ff9e64' },
           },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
+          -- { "encoding" },
+          -- { "fileformat" },
+          { 'filetype' },
         },
       },
     })
   end,
 }
+
+return M
