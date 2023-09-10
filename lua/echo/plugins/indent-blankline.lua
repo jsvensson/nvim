efa29348fd2e79647ca7ed1cbe8a260fbd0124fd
Johan Svensson <johan@reducer.io>
2023-09-10 23:09:55 +0200

    indent: adjust settings

--- lua/echo/plugins/indent-blankline.lua
+++ lua/echo/plugins/indent-blankline.lua
@@ -6,6 +6,8 @@ local M = {
       char = '┊',
       show_current_context = true, -- highlight the current context
       show_current_context_start = false, -- underline on starting line for context
+      indent_blankline_max_indent_increase = 1, -- don't show char in multiline strings
+      indent_blankline_show_trailing_blankline_indent = false, -- no char on empty lines in current context
     })
   end,
 }
