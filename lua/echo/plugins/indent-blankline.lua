d25cbcc329826956e69640d7e96dab0d12c3bc8e
Johan Svensson <johan@reducer.io>
2023-09-10 23:31:15 +0200

    indent: fix trailing blankline indent

--- lua/echo/plugins/indent-blankline.lua
+++ lua/echo/plugins/indent-blankline.lua
@@ -1,13 +1,15 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
+  event = "BufRead",
   config = function()
+    -- for unclear reasons this won't work if applied inside the setup
+    vim.g.indent_blankline_show_trailing_blankline_indent = false
+
     require("indent_blankline").setup({
       char = '┊',
       show_current_context = true, -- highlight the current context
       show_current_context_start = false, -- underline on starting line for context
       indent_blankline_max_indent_increase = 1, -- don't show char in multiline strings
-      indent_blankline_show_trailing_blankline_indent = false, -- no char on empty lines in current context
     })
   end,
 }
