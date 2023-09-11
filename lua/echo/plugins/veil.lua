617055692b185a4ae05e3f5880b34baeae2141b2
Johan Svensson <johan@reducer.io>
2023-09-11 21:54:39 +0200

    update plugins; add veil.nvim

---
+++ lua/echo/plugins/veil.lua
@@ -0,0 +1,12 @@
+local M = {
+  'willothy/veil.nvim',
+  lazy = false,
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim"
+  },
+  config = true,
+}
+
+return M
