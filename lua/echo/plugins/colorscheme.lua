43161525c38fa0c31f8276656e5dbc019a8e9235
Johan Svensson <johan@reducer.io>
2023-09-11 20:17:30 +0200

    handle Ayu dark/light mode switching

--- lua/echo/plugins/colorscheme.lua
+++ lua/echo/plugins/colorscheme.lua
@@ -2,27 +2,36 @@ local M = {
   'Shatur/neovim-ayu',
   priority = 1000, -- load before all other plugins
   config = function()
-    local panel_bg = '#212733' -- not a part of Ayu Dark
     local colors = require('ayu.colors')
     colors.generate()
 
     require('ayu').setup({
       mirage = false,
       -- Transparency overrides, see https://github.com/Shatur/neovim-ayu#transparency
-      overrides = {
-        NormalFloat = { bg = panel_bg }, -- lighter UI popups
-        WinSeparator = { fg = colors.selection_bg }, -- border between windows
+      overrides = function()
+        -- dark mode color adjustments
+        local panel_bg = '#212733' -- not a part of Ayu Dark
 
-        -- adjust line numbers in gutter
-        LineNr = { fg = colors.func },
-        LineNrAbove = { fg = colors.selection_bg },
-        LineNrBelow = { fg = colors.selection_bg },
+        -- light mode color adjustments
+        if vim.o.background == 'light' then
+          panel_bg = '#f0f0f0'
+        end
 
-        NeoTreeCursorLine = { bg = panel_bg },
-      },
+        return {
+          NormalFloat = { bg = panel_bg }, -- adjust UI popups
+
+          -- adjust line numbers in gutter
+          LineNr = { fg = colors.func },
+          LineNrAbove = { fg = colors.selection_bg },
+          LineNrBelow = { fg = colors.selection_bg },
+
+          WinSeparator = { fg = colors.selection_bg }, -- border between windows
+          NeoTreeCursorLine = { bg = colors.selection_bg },
+        }
+      end,
     })
 
-  vim.cmd([[colorscheme ayu]])
+    vim.cmd([[colorscheme ayu]])
   end,
 }
 
