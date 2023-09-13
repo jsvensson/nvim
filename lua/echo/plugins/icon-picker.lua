feaec4295d016d33bcf263f0de591cd12479b817
Johan Svensson <johan@reducer.io>
2023-09-13 19:16:29 +0200

    remap icon-picker
    Ctrl-S for "symbol"

--- lua/echo/plugins/icon-picker.lua
+++ lua/echo/plugins/icon-picker.lua
@@ -6,8 +6,8 @@ local M = {
   config = function()
     local opts = { noremap = true, silent = true }
 
-    vim.keymap.set('i', '<C-i>', '<Cmd>IconPickerInsert emoji nerd_font_v3 alt_font symbols<CR>', opts)
-    vim.keymap.set('n', '<C-i>', '<Cmd>IconPickerYank emoji nerd_font_v3 alt_font symbols<CR>', opts)
+    vim.keymap.set('i', '<C-s>', '<Cmd>IconPickerInsert emoji nerd_font_v3 alt_font symbols<CR>', opts)
+    vim.keymap.set('n', '<C-s>', '<Cmd>IconPickerYank emoji nerd_font_v3 alt_font symbols<CR>', opts)
 
     require('icon-picker').setup({
       disable_legacy_commands = true,
