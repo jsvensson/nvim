716ec6db01bcfc62a7d75d796b90e8466d82bca4
Johan Svensson <johan@reducer.io>
2023-09-16 23:56:52 +0200

    move color schemes to separate dir

--- lua/echo/lazy.lua
+++ lua/echo/lazy.lua
@@ -16,8 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { import = "echo.plugins" },
   { import = "echo.plugins.lsp" },
+  { import = "echo.plugins.colorschemes" },
 }, {
-  install = {
-    colorscheme = { "ayu" },
-  },
+  install = {},
 })
