e36dd76bd7b5b3cfd734fb1c4dc0eed637935be3
Johan Svensson <johan@reducer.io>
2023-09-18 23:48:04 +0200

    add delete buffer command

--- lua/echo/plugins/telescope.lua
+++ lua/echo/plugins/telescope.lua
@@ -14,9 +14,13 @@ return {
       defaults = {
         mappings = {
           i = {
+            ["<C-d>"] = actions.delete_buffer,
             ["<C-k>"] = actions.move_selection_previous,
             ["<C-j>"] = actions.move_selection_next,
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+          n = {
+            ["<C-d>"] = actions.delete_buffer,
           },
         },
       },
