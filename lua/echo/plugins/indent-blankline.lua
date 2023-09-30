28d44bdeb3552159940bbb52fb77c2257654323a
Johan Svensson <johan@reducer.io>
2023-10-01 00:46:31 +0200

    migrate to v3

--- lua/echo/plugins/indent-blankline.lua
+++ lua/echo/plugins/indent-blankline.lua
@@ -3,14 +3,13 @@ local M = {
   main = "ibl",
   event = "BufRead",
   config = function()
-    -- for unclear reasons this won't work if applied inside the setup
-    vim.g.indent_blankline_show_trailing_blankline_indent = false
-
     require("ibl").setup({
-      char = "┊",
-      show_current_context = true, -- highlight the current context
-      show_current_context_start = false, -- underline on starting line for context
-      indent_blankline_max_indent_increase = 1, -- don't show char in multiline strings
+      indent = {
+        char = "┊",
+      },
+      scope = {
+        enabled = true,
+      },
     })
   end,
 }
