bc37ea24ebfebf35e9daebd3f9074537da7aeeba
Johan Svensson <johan@reducer.io>
2023-10-01 00:05:12 +0200

    chore: update to v3

--- lua/echo/plugins/indent-blankline.lua
+++ lua/echo/plugins/indent-blankline.lua
@@ -1,11 +1,12 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = "BufRead",
   config = function()
     -- for unclear reasons this won't work if applied inside the setup
     vim.g.indent_blankline_show_trailing_blankline_indent = false
 
-    require("indent_blankline").setup({
+    require("ibl").setup({
       char = "┊",
       show_current_context = true, -- highlight the current context
       show_current_context_start = false, -- underline on starting line for context
