d1d4fd60b427466e87982bd6b1ee866669049e89
Johan Svensson <johan.svensson@detectify.com>
2023-10-17 14:51:23 +0200

    adjust line number colors

--- lua/echo/plugins/colors/rose-pine.lua
+++ lua/echo/plugins/colors/rose-pine.lua
@@ -4,6 +4,14 @@ local M = {
 }
 
 M.config = function()
+  require("rose-pine").setup({
+    highlight_groups = {
+      LineNr = { fg = "gold" },
+      LineNrAbove = { fg = "muted" },
+      LineNrBelow = { fg = "muted" },
+    },
+  })
+
   vim.cmd([[colorscheme rose-pine-main]])
 end
 
