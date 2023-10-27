53f30ca5c354ed8c550daf89140eb6703782978d
Johan Svensson <johan@reducer.io>
2023-10-28 01:37:16 +0200

    replace LuaSnip

--- lua/echo/plugins/nvim-cmp.lua
+++ lua/echo/plugins/nvim-cmp.lua
@@ -1,27 +1,27 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
   dependencies = {
+    -- completion engine
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    -- sources
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
-    -- "l3MON4D3/LuaSnip", -- snippet engine
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "hrsh7th/cmp-cmdline", -- source for command line
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-nvim-lsp", -- source for nvim lsp
     "rafamadriz/friendly-snippets", -- snippet collection
   },
   config = function()
     local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    -- loads snippets from installed plugins
-    require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      snippet = { -- configure now nvim-cmp interacts with snippet engine
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
-          luasnip.lsp_expand(args.body)
+          vim.fn["vsnip#anonymous"](args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
@@ -29,16 +29,38 @@ return {
         ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show suggestions
+        ["<C-c>"] = cmp.mapping.complete(), -- show suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
+        { name = "cmdline" }, -- command line
       }),
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+    })
+
+    -- Use cmdline & path source for `:`
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+
+    -- Use buffer source for `/` and `?`
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
     })
   end,
 }
