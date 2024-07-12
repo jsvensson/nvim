7f0dd0d4a4fbe5cb0f14ec0c64c3f1037aee4b89
Johan Svensson <johan@reducer.io>
2024-07-13 00:14:39 +0200

    telescope-frecency issue now fixed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -40,9 +40,6 @@ M.config = function()
     extensions = {
       frecency = {
         show_scores = true,
-        -- issue where files under .git are still shown
-        -- https://github.com/nvim-telescope/telescope-frecency.nvim/issues/211
-        workspace_scan_cmd = "LUA",
       },
     },
   })
