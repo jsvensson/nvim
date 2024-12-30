9a3fe70fcf78d066ec012ae2854296466d08536b
Johan Svensson <johan@spiralmind.io>
2024-12-30 02:05:54 +0100

    rose-pine theme: remove bg colors
    In order to make the background transparent in the terminal.

--- lua/plugins/colors/rose-pine.lua
+++ lua/plugins/colors/rose-pine.lua
@@ -6,6 +6,10 @@ local M = {
 M.config = function()
   require("rose-pine").setup({
     highlight_groups = {
+      -- Change background colors so they're transparent in term
+      Normal = { bg = "NONE" },
+      NormalNC = { bg = "NONE" },
+
       LineNr = { fg = "gold" },
       LineNrAbove = { fg = "muted" },
       LineNrBelow = { fg = "muted" },
