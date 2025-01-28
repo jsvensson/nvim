cf2348a296aff7e5dca0abbdb3c27c771d787582
Johan Svensson <johan@reducer.io>
2025-01-28 18:29:46 +0100

    add autocmd to run tofu fmt on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_augroup("OpenTofu", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = "OpenTofu",
+  pattern = "*.tf",
+  callback = function()
+    os.execute("tofu fmt --list=false")
+    vim.cmd("edit!")
+  end,
+})
